cd44b716d9998ac48d3d87efe4b03cf9674c9410
FireKotlin <fire3456gamer@gmail.com>
2023-08-28 16:44:23 -0700

    upload

--- minikube/generate_metrics02.py
+++ minikube/generate_metrics02.py
@@ -1,4 +1,4 @@
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Gauge, Counter
 import random
 import time
 
@@ -12,10 +12,20 @@ def generate_metric():
         gauge_metric.set(value)
         time.sleep(5)  # Actualizar la métrica cada 5 segundos
 
+counter_metric = Counter('my_count_metric', 'test')
+
+def generate_counter_mtr():
+#   value = 1.5
+    while counter_metric < 30:
+        counter_metric.inc(1.6)
+        time.sleep(5)
+        
+
 if __name__ == '__main__':
     # Iniciar el servidor HTTP para exponer las métricas
     start_http_server(port=9091)
 
     # Generar y actualizar la métrica en segundo plano
     generate_metric()
+    generate_counter_mtr() 
 
